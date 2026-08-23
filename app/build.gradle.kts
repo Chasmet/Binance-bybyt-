8e9e0f4de4305897df9081c7b83342e7b1409589
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-08-23 21:14:33 +0200

    release: CHK Crypto 0.6.2 local keys

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.chk.binancebybit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 16
-        versionName = "0.6.1"
+        versionCode = 17
+        versionName = "0.6.2"
     }
 
     signingConfigs {
