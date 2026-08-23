d4d6185ed76908d460297ec8a6fb3ea6adf653a9
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-08-23 19:23:59 +0200

    chore: bump CHK Crypto to 0.5.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.chk.binancebybit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "0.5.3"
+        versionCode = 13
+        versionName = "0.5.4"
     }
 
     buildTypes {
