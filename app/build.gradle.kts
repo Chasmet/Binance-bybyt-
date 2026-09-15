7a7dbb6e7e12b34fd7aaa489dad94de0408bfb5b
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-15 06:21:22 +0200

    chore(release): bump CHK Crypto to 0.11.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.chk.binancebybit"
         minSdk = 26
         targetSdk = 35
-        versionCode = 36
-        versionName = "0.10.0"
+        versionCode = 37
+        versionName = "0.11.0"
     }
 
     signingConfigs {
